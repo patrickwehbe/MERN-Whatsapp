e4029089725034e3dc301db461910aed1a87d40a
Patrick Wehbe <pew02@mail.aub.edu>
2020-09-13 17:12:48 +0300

    chat header

--- mern-whatsapp-frontend/src/Chat.js
+++ mern-whatsapp-frontend/src/Chat.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVert from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
 
 function Chat() {
   return (
     <div className="chat">
-      <h2>I am a a chat</h2>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body"></div>
     </div>
   );
 }
